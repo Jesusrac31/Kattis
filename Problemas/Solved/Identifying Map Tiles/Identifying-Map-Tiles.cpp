e0f09f3f3b895a4eb3ae6002f6e00ecbbd72d1c9
Jesusrac31 <jesusraceros@gmail.com>
2025-04-08 11:41:31 +0200

    8-4-2025-11:41

---
+++ Problemas/Solved/Identifying Map Tiles/Identifying-Map-Tiles.cpp
@@ -0,0 +1,123 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<long long int> vll;
+typedef long long int lli;
+typedef pair<int, int> pii;
+typedef map<string, int> msi;
+typedef map<int, vector<int>> miv;
+
+// Funciones vector
+#define PB(a) emplace_back(a);
+
+bool sort_func(int a, int b) {
+    if (a < b) {
+        return true;
+    } else {
+        return false;
+    }
+}
+#define ord(vect) sort(vect.begin(), vect.end(), sort_func);
+
+#define borra_el(vect, el) vect.erase(vect.find(el));
+#define borra_range(vect, a, b) vect.erase(a, b);
+#define borra(vect, n) vect.erase(vect.begin() + n);
+#define B begin();
+#define E end();
+#define copia(v1, v2)                                                                                                                    \
+    ;                                                                                                                                                        \
+    copy(v1.begin(), v1.end(), back_inserter(v2));
+
+// Funciones map
+#define F first;
+#define S second;
+
+// Logaritmo de 2
+double log_2 = log(2);
+double log2(int a) { return (log(a) / log_2); }
+
+void Imprime(vi vect) {
+    for (int i = 0; i < vect.size(); i++) {
+        cout << vect[i] << " ";
+    }
+    cout << "\n";
+}
+
+void Imprime2d(vector<vi> vect) {
+    for (int j = 0; j<vect.size(); j++){
+        for (int i = 0; i < vect[j].size(); i++) {
+                cout << vect[j][i] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void Imprime_set(set<int> s) {
+    copy(s.begin(), s.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+}
+
+int maximo_comun_divisor(int a, int b) {
+    int temporal; // Para no perder b
+    while (b != 0) {
+        temporal = b;
+        b = a % b;
+        a = temporal;
+    }
+    return a;
+}
+
+int minimo_comun_multiplo(int a, int b) {
+    return (a * b) / maximo_comun_divisor(a, b);
+}
+
+bool isNumeric(string const &str) {
+    auto it = str.begin();
+    while (it != str.end() && isdigit(*it)) {
+        it++;
+    }
+    return !str.empty() && it == str.end();
+}
+
+vi lee(int n) {
+    int el;
+    vi vect;
+    for (int i = 0; i < n; i++) {
+        cin >> el;
+        vect.PB(el);
+    }
+    return (vect);
+}
+
+int solve() {
+    // Code aquí
+    string quadkey;
+    cin >> quadkey;
+    int level, x=0, y=0;
+    level = quadkey.size();
+    int part = pow(2, level-1);
+    for (int i = 0; i<level; i++){
+        if (quadkey[i] == '1'){
+            x += part;
+        } else if (quadkey[i] == '2'){
+            y += part;
+        } else if (quadkey[i] == '3'){
+            x += part;
+            y += part;
+        }
+        part/=2;
+    }
+    cout << level << " " << x << " " << y << endl;
+
+    return 0;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr); 
+    solve();
+    return 0;
+}
